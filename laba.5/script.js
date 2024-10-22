648616445895a75ec8d617b4dd1adda914f0304c
Zasoid <grimmhisage@gmail.com>
2024-10-22 19:14:48 +0300

    Delete laba.5 directory

--- laba.5/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('changeTextButton').addEventListener('click', function() {
-    const paragraph = document.getElementById('text');
-    paragraph.textContent = 'ОН СТАЛ БРАЗИЛЙСКОЙ КАПУСТОЙ!'; // Изменяем текст
-    paragraph.style.color = 'red'; // Меняем цвет текста на красный
-});
